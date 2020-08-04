48389f606762671ab9bcab5f5ab911ecd21e0ac1
Christopher Lund <christopher.lund@pearson.com>
2020-08-04 16:58:20 -0400

    Add page to provide WarcraftLog url or report code
    * Created component for entering either url or report code for the WarcraftLog to be parsed.

---
+++ src/components/LogSelection.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Card, Input, Message } from 'semantic-ui-react';
+
+const LogSelection = () => {
+	const [logCode, setLogCode] = useState("");
+	const [hasError, setHasError] = useState(false);
+
+	const handleOnClick = () => {
+		const regex = RegExp('^(https://classic.warcraftlogs.com/reports/){0,1}[a-zA-Z0-9]{16}[#]{0,1}[a-zA-Z0-9#=&-/]{0,}$');
+		setHasError(!regex.test(logCode));
+	}
+
+	return (
+		<Card centered fluid raised color='green'>
+			<Card.Content>
+				<Card.Header>Enter the Classic WarcraftLogs url or report code you wish to analyze.</Card.Header>
+				<Card.Description textAlign='center'>
+					<Input style={{ width: '75%' }} action={{ icon: 'search', onClick: handleOnClick }} 
+								onChange={ (e, data) => { setLogCode(data.value) } } error={hasError}
+								placeholder='https://classic.warcraftlogs.com/reports/<report code>' />
+					<Message negative compact hidden={!hasError}>
+						WarcraftLog url or report code is invalid
+					</Message>
+				</Card.Description>
+			</Card.Content>
+		</Card>
+	)
+}
+
+export default LogSelection;
